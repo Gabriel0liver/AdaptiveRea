c21bdc448d423ddc75183abe0a26bbb8c3cc1627
PabloArrNowak <82083153+PabloArrNowak@users.noreply.github.com>
2025-04-25 10:09:27 +0200

    Conditional Jumps UI (Done)

--- ReaGoTo/Functions/Conditional GoTo Functions.lua
+++ ReaGoTo/Functions/Conditional GoTo Functions.lua
@@ -1,11 +1,9 @@
 _G.ConditionalJumps = _G.ConditionalJumps or {}
-ConditionalJumps = _G.ConditionalJumps  -- use shared global table
 
 _G_AdaptiveParams = _G_AdaptiveParams or {}
 local AdaptiveParams = _G_AdaptiveParams
 
 -- Update param values, called by AdaptiveHost
--- Desde AdaptiveHost: SetAdaptiveParamValue(param_name, value)
 function SetAdaptiveParamValue(param_name, value)
     AdaptiveParams[param_name] = tonumber(value)
 end
@@ -23,7 +21,7 @@ end
 
 -- Check, perform jumps
 function CheckConditionalJumps(current_region)
-    for _, rule in ipairs(ConditionalJumps) do
+    for _, rule in ipairs(_G.ConditionalJumps or {}) do
         if rule.from == current_region then
             local param_value = AdaptiveParams[rule.param]
             if param_value and EvaluateCondition(param_value, rule.op, tonumber(rule.value)) then
@@ -49,7 +47,7 @@ function PlaylistAndConditionalJumpUI(playlist, ctx)
 
     -- Bottom half: Conditional jumps
     if reaper.ImGui_BeginChild(ctx, "ConditionalJumps", avail_x, -FLTMIN, true) then
-        ConditionalJumpsUI(ctx)
+        ConditionalJumpsUI(ctx, playlist)
         reaper.ImGui_EndChild(ctx)
     end
 
@@ -57,79 +55,92 @@ function PlaylistAndConditionalJumpUI(playlist, ctx)
 end
 
 -- Conditional Jumps UI
-function ConditionalJumpsUI(ctx)
+function ConditionalJumpsUI(ctx, playlist)
     reaper.ImGui_Separator(ctx)
     reaper.ImGui_Text(ctx, 'Conditional Jumps')
 
     if reaper.ImGui_Button(ctx, '+') then
-        table.insert(ConditionalJumps, {
+        _G.ConditionalJumps[#_G.ConditionalJumps + 1] = {
             from = '',
             to = '',
             param = '',
             op = '>',
             value = '0',
-        })
-        reaper.ShowConsoleMsg("Total conditional jumps: " .. tostring(#ConditionalJumps) .. "\n")
+        }
     end
 
-    local playlist = ProjConfigs[FocusedProj] and ProjConfigs[FocusedProj].playlists and ProjConfigs[FocusedProj].playlists[ProjConfigs[FocusedProj].current]
     if not playlist then return end
+    
+     -- Gather region names for 'from'
+     local from_names = {}
+     for i = 0, reaper.CountProjectMarkers(0) - 1 do
+         local _, isrgn, _, _, name = reaper.EnumProjectMarkers(i)
+         if isrgn == true and name and name ~= '' then
+             table.insert(from_names, name)
+         end
+     end
+ 
+     -- Gather all region/marker names from the project for 'to'
+     local to_names = {}
+     for i = 0, reaper.CountProjectMarkers(0) - 1 do
+         local _, _, _, _, name = reaper.EnumProjectMarkers(i)
+         if name and name ~= '' then
+             table.insert(to_names, name)
+         end
+     end
+ 
+     for i = #_G.ConditionalJumps, 1, -1 do
+         local jump = _G.ConditionalJumps[i]
+ 
+         reaper.ImGui_PushID(ctx, i)
+ 
+         -- FROM region (no arrow button)
+         reaper.ImGui_SetNextItemWidth(ctx, 60)
+         if reaper.ImGui_BeginCombo(ctx, '##From'..i, jump.from ~= '' and jump.from or 'Any', reaper.ImGui_ComboFlags_NoArrowButton()) then
+             if reaper.ImGui_Selectable(ctx, 'Any', jump.from == 'Any') then
+                 jump.from = 'Any'
+             end
+             for _, name in ipairs(from_names) do
+                 if reaper.ImGui_Selectable(ctx, name, jump.from == name) then
+                     jump.from = name
+                     if jump.to == name then
+                         jump.to = ''
+                     end
+                 end
+             end
+             reaper.ImGui_EndCombo(ctx)
+         end
+ 
+         reaper.ImGui_SameLine(ctx)
+         reaper.ImGui_Text(ctx, 'to')
+         reaper.ImGui_SameLine(ctx)
+ 
+         -- TO region or marker (excluding same as FROM)
+         reaper.ImGui_SetNextItemWidth(ctx, 60)
+         if reaper.ImGui_BeginCombo(ctx, '##To'..i, jump.to ~= '' and jump.to or '', reaper.ImGui_ComboFlags_NoArrowButton()) then
+             for _, name in ipairs(to_names) do
+                 if name ~= jump.from then
+                     if reaper.ImGui_Selectable(ctx, name, jump.to == name) then
+                         jump.to = name
+                     end
+                 end
+             end
+             reaper.ImGui_EndCombo(ctx)
+         end
+ 
+         reaper.ImGui_SameLine(ctx)
+         reaper.ImGui_Text(ctx, 'if')
+         reaper.ImGui_SameLine(ctx)
 
-    local region_names = {}
-    for i, region in ipairs(playlist) do
-        if region.name and region.name ~= '' then
-            table.insert(region_names, region.name)
-        end
-    end
-
-    for i = #ConditionalJumps, 1, -1 do
-        local jump = ConditionalJumps[i]
-        reaper.ShowConsoleMsg("Showing jump " .. i .. "\n")
-
-        reaper.ImGui_PushID(ctx, i)
-
-        reaper.ImGui_SetNextItemWidth(ctx, 100)
-        if reaper.ImGui_BeginCombo(ctx, 'From##'..i, jump.from ~= '' and jump.from or 'Select region') then
-            if reaper.ImGui_Selectable(ctx, 'Any', jump.from == 'Any') then
-                jump.from = 'Any'
-            end
-            for _, name in ipairs(region_names) do
-                if reaper.ImGui_Selectable(ctx, name, jump.from == name) then
-                    jump.from = name
-                    if jump.to == name then
-                        jump.to = ''
-                    end
-                end
-            end
-            reaper.ImGui_EndCombo(ctx)
-        end
-
+        -- PARAM name input
+        reaper.ImGui_SetNextItemWidth(ctx, 20)
+        _, jump.param = reaper.ImGui_InputText(ctx, '##Param'..i, jump.param or '')
         reaper.ImGui_SameLine(ctx)
-        reaper.ImGui_Text(ctx, '→')
-        reaper.ImGui_SameLine(ctx)
 
-        reaper.ImGui_SetNextItemWidth(ctx, 100)
-        if reaper.ImGui_BeginCombo(ctx, 'To##'..i, jump.to ~= '' and jump.to or 'Select region') then
-            for _, name in ipairs(region_names) do
-                if name ~= jump.from then
-                    if reaper.ImGui_Selectable(ctx, name, jump.to == name) then
-                        jump.to = name
-                    end
-                end
-            end
-            reaper.ImGui_EndCombo(ctx)
-        end
-
-        reaper.ImGui_Text(ctx, 'if')
-        reaper.ImGui_SameLine(ctx)
-
-        reaper.ImGui_SetNextItemWidth(ctx, 80)
-        _, jump.param = reaper.ImGui_InputText(ctx, 'Param##'..i, jump.param or '')
-        reaper.ImGui_SameLine(ctx)
-
+        -- OPERATOR combo (no arrow button)
         local operators = { '>', '<', '>=', '<=', '==', '!=' }
-        reaper.ImGui_SetNextItemWidth(ctx, 40)
-        if reaper.ImGui_BeginCombo(ctx, 'Op##'..i, jump.op or '>') then
+        reaper.ImGui_SetNextItemWidth(ctx, 20)
+        if reaper.ImGui_BeginCombo(ctx, '##Op'..i, jump.op or '>', reaper.ImGui_ComboFlags_NoArrowButton()) then
             for _, op in ipairs(operators) do
                 if reaper.ImGui_Selectable(ctx, op, jump.op == op) then
                     jump.op = op
@@ -137,12 +148,15 @@ function ConditionalJumpsUI(ctx)
             end
             reaper.ImGui_EndCombo(ctx)
         end
+
         reaper.ImGui_SameLine(ctx)
 
-        reaper.ImGui_SetNextItemWidth(ctx, 50)
-        _, jump.value = reaper.ImGui_InputText(ctx, 'Value##'..i, tostring(jump.value or ''))
+        -- VALUE input
+        reaper.ImGui_SetNextItemWidth(ctx, 20)
+        _, jump.value = reaper.ImGui_InputText(ctx, '##Value'..i, tostring(jump.value or ''))
         reaper.ImGui_SameLine(ctx)
 
+        -- Transition and Delete buttons
         if reaper.ImGui_Button(ctx, 'Transition##'..i) then
             if jump.from and jump.from ~= "Any" then
                 current_region = jump.from
@@ -152,8 +166,8 @@ function ConditionalJumpsUI(ctx)
         end
 
         reaper.ImGui_SameLine(ctx)
-        if reaper.ImGui_Button(ctx, '🗑##'..i) then
-            table.remove(ConditionalJumps, i)
+        if reaper.ImGui_Button(ctx, 'X##'..i) then
+            table.remove(_G.ConditionalJumps, i)
             reaper.ImGui_PopID(ctx)
             goto continue
         end
